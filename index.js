28102890826e401e17e33175973278b1b08a3d2c
DJ Hersh <DanielJHersh@gmail.com>
2020-11-21 15:00:24 -0800

    Added comments to my index.js file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+// Declaring variables for the imported files I will be using to create the README file
 const inquirer = require("inquirer");
 const fs = require('fs');
 
+// The questions a user will answer
 inquirer.prompt([
     {
         type: 'input',
@@ -49,7 +51,11 @@ inquirer.prompt([
         name: 'email'
     }
 ]).then((response)=> {
+
+    // Takes the title of the user's project and puts dashes in place of any spaces to be used in the README file name
     const title = response.title.split(" ").join("-");
+
+    // Where the file gets created using the information from the user
     fs.writeFile(`${title}-README.md`, `# ${response.title}
 
 ![GitHub License](https://img.shields.io/badge/license-${response.license}-blue.svg)
@@ -94,5 +100,7 @@ https://github.com/${response.github}
     
 For any questions, please email me at:
 ${response.email}`, (err)=>
+
+    // If there is an error at any point in the process, it will be printed to the console/terminal, otherwise a success message will be printed
     err ? console.log(err) : console.log("You've created a new README file!"))
 });
